18e2bebc7d4409a70838c51281e50b9ab81094a9
K-Nakagawa1210 <n349517k@gmail.com>
2025-10-31 16:23:46 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:club_name, :address, :phone_number])
     devise_parameter_sanitizer.permit(:account_update, keys: [:club_name, :address, :phone_number])
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
   
 end
